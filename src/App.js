61072dc0f949a2953f36ac6f9b0e82e04b7733f9
AbhishekBaliyanWork <abhishek.baliyan.work@gmail.com>
2018-10-21 16:14:09 +0530

    Learned Props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Sniper")
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt My Dog!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, {
+      name: "Anny",
+      animal: "Ants",
+      breed: "Fire Ant"
+    }),
+    React.createElement(Pet, {
+      name: "Nitin",
+      animal: "monkey",
+      breed: "Gelada"
+    }),
+    React.createElement(Pet, {
+      name: "Mohan",
+      animal: "dog",
+      breed: "Bulldog"
+    })
   ]);
 };
 
