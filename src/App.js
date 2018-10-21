95ab0fc1438cfdd0799703f810dd08239705d81b
AbhishekBaliyanWork <abhishek.baliyan.work@gmail.com>
2018-10-21 16:18:12 +0530

    Change function to class

--- src/App.js
+++ src/App.js
@@ -6,25 +6,48 @@ const Pet = (props) => {
   ]);
 };
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt My Dog!"),
-    React.createElement(Pet, {
-      name: "Anny",
-      animal: "Ants",
-      breed: "Fire Ant"
-    }),
-    React.createElement(Pet, {
-      name: "Nitin",
-      animal: "monkey",
-      breed: "Gelada"
-    }),
-    React.createElement(Pet, {
-      name: "Mohan",
-      animal: "dog",
-      breed: "Bulldog"
-    })
-  ]);
-};
+class App extends React.Component {
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt My Dog!"),
+      React.createElement(Pet, {
+        name: "Anny",
+        animal: "Ants",
+        breed: "Fire Ant"
+      }),
+      React.createElement(Pet, {
+        name: "Nitin",
+        animal: "monkey",
+        breed: "Gelada"
+      }),
+      React.createElement(Pet, {
+        name: "Mohan",
+        animal: "dog",
+        breed: "Bulldog"
+      })
+    ]);  
+  }
+}
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt My Dog!"),
+//     React.createElement(Pet, {
+//       name: "Anny",
+//       animal: "Ants",
+//       breed: "Fire Ant"
+//     }),
+//     React.createElement(Pet, {
+//       name: "Nitin",
+//       animal: "monkey",
+//       breed: "Gelada"
+//     }),
+//     React.createElement(Pet, {
+//       name: "Mohan",
+//       animal: "dog",
+//       breed: "Bulldog"
+//     })
+//   ]);
+// };
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
