f7261eaacef7f9d40f7b610bb8bb848dab25160d
AbhishekBaliyanWork <abhishek.baliyan.work@gmail.com>
2018-10-22 16:29:07 +0530

    Remove Class Constructor with Babel

--- src/Details.js
+++ src/Details.js
@@ -8,13 +8,16 @@ const petfinder = pf({
 });
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    loading: true
+  };
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      loading: true
-    };
-  }
+  //   this.state = {
+  //     loading: true
+  //   };
+  // }
   componentDidMount() {
     petfinder.pet
       .get({
